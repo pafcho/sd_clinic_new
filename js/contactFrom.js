355c7f233a113193e409a29bdecb5835b2e6a0c7
pafcho <rubiev.pavel@gmail.com>
2020-05-19 15:42:18 +0300

    Contact Phorm done

---
+++ js/contactFrom.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+	$('#submitForm').click(function (event) {
+		event.preventDefault();
+		
+		var email = #('#email').val();
+		var name = #('#name').val();
+		var message = #('#message').val();
+	})
+})
